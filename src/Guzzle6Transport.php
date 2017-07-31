6e3d0834801191429882ab3ed03c80be79545651
Yaroslav Lukyanov <c_sharp@mail.ru>
2017-07-31 14:46:40 +0700

    remove merge recursively, just override needed params

--- src/Guzzle6Transport.php
+++ src/Guzzle6Transport.php
@@ -30,7 +30,8 @@ class Guzzle6Transport implements Transport
      */
     public function __construct(array $config = [], Client $client = null)
     {
-        $config = array_merge_recursive(['base_uri' => 'http://127.0.0.1:8200', 'http_errors' => false], $config);
+        // merge and override http errors settings
+        $config = array_merge(['base_uri' => 'http://127.0.0.1:8200'], $config, ['http_errors' => false]);
 
         $this->client = $client ?: new Client($config);
     }
